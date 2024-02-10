b386da42ed12d3f9116f9c5c736258b0ae140094
Dean Uziel <92694814+Dean99712@users.noreply.github.com>
2024-02-10 23:07:41 +0200

    updated functions and UI, changes in navigation bar added icons

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
             element: <Root/>,
             children: [
                 {
-                    path: '/games',
+                    path: '/',
                     element: <EntryFormPage/>
                 },
                 {
